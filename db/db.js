1702085fbbb3324de9322944b98550a32b2962c1
dhruv161 <dhruvpandya16199@gmail.com>
2022-12-06 22:07:46 +0530

    intial commiy

---
+++ db/db.js
@@ -0,0 +1,251 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Dhryv Pandya. I’m currently working as a Software Engineer at AyanWorks Technology Solutions Pvt. Ltd in React Native Developer. I did my graduation in IT from GTU from gandhinagar.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love play cricket, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: dhruvpandya16199@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JavaScript, TypeScript, Python, C++",
+			color: "1",
+			percentage: "70",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "React Native, Redux, Hooks, Saga, Flutter",
+			color: "6",
+			percentage: "80",
+		},
+		{
+			title: "Clouds",
+			skillName: "Firebase, AWS(EC2, S3), Heroku, Netlify",
+			color: "3",
+			percentage: "60",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "4",
+			percentage: "50",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, Bitbucket, JIRA, Trello",
+			color: "7",
+			percentage: "80",
+		},
+		{
+			title: "Tools",
+			skillName: "Xcode, Android Studio, VS Code, Postman,Reactotron, Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "Operating Systems",
+			skillName: "Linux, Macos, Windows",
+			color: "2",
+			percentage: "80",
+		},
+	],
+	projects: {
+		mobile: [
+			{
+				projectName: "Transportation Management System",
+				summary: "Kargo is here to transform Indonesia's $ 250 billion logistics industry. Indonesia's supply chain process still relies on telephone and paper. Kargo is here to answer Indonesia logistics problems with technology.",
+				preview: "https://kargo.tech/",
+				techStack: [
+					"React Native", 
+					"Context", 
+					"Navigation", 
+					"Google MAP Api integration", 
+					"Google Analytics",
+					"GraphQL", 
+					"i18 internationalization",
+				],
+			},
+			{
+				projectName: "Mngle",
+				summary: "Mngle is an online dating application providing dating service presented through a mobile phone application, often taking advantage of a smartphone’s GPS location capabilities, always on-hand presence, and easy access to digital photo galleries and mobile wallets to enhance the traditional nature of online dating.",
+				preview: "https://play.google.com/store/apps/details?id=com.mngle.app",
+				techStack: ["React Native", "Saga", "Redux", "Hooks", "Navigator", "Firebase", "Realtime Chat"],
+			},
+			{
+				projectName: "Smiles & Tears",
+				summary:
+					"Nelson’s Journey supports children and young people in Norfolk who’ve experienced the death of a significant person. We provide a range of resources and services, and accept referrals from families and professionals for those who may need our support.",
+				preview: "https://apps.apple.com/gb/app/smiles-tears/id965830012",
+				techStack: ["React Native", "Saga", "Redux", "Hooks", "Navigator"],
+			},
+			{
+				projectName: "Food jugaad",
+				summary:
+					"Digital solution for Restaurant Management. FoodJugaad digitizes the waitlist of your favorite restaurant and make it available on your mobile phones. Check waiting at your favorite restaurant and ‘Reserve Time’ to avoid waiting and truly enjoy the restaurant experience.",
+				preview: "https://play.google.com/store/apps/details?id=fjmanager.com",
+				techStack: ["React Native", "Saga", "Redux", "Hooks", "Navigator", "Figma", "Firebase", "Social Login Facebook & Google", "Google Analytics"],
+			},
+		],
+		freelance: [
+			{
+				projectName: "EMS Medic & Assist APP",
+				summary:
+					"We have developed application which is helping people who are attending any event and during that time in any emergency they can use this app.",
+				techStack: ["React Native", "Mobx", "Hooks", "Navigator", "Figma", "Firebase", "Social Login Facebook & Google", "Google Analytics"],
+			},
+			{
+				projectName: "Orion",
+				summary:
+					"Assisted in Developing a react native app where anybody could order clothes online and PO print via thermal printer",
+				techStack: ["React Native", "Mobx", "Hooks", "Navigator", "Play Store"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "AyanWorks Technology Solutions Pvt. Ltd.",
+			duration: "Sept 2022 - Present",
+			subtitle: "React-Native Developer",
+			details: [
+				"ARIES Protocols.",
+				"Worked on Indy SDK for store the digital credential in the indy based wallet",
+				'Experienced in React Native for hybrid app development',
+				'Self Sovereign Identity Solutions.'
+			],
+			tags: [
+				"JavaScript",
+				"TypesScript",
+				"React-Native",
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Nstack Softech LLP",
+			duration: "December 2018 - August 2022",
+			subtitle: "React-Native Developer",
+			details: [
+				"Swipe gesture for liking and disliking users",
+				"Chat implementation(Firebase real-time)",
+				"Profile Management",
+				"Location based users suggestion",
+				"Share restaurants over chat",
+				"Listing restaurants based on your location",
+				"Implemented SAGA and REDUX",
+				"Notification implementation",
+				"Implemented React Context and Hooks",
+				"Custom Component development",
+				"Apollo GraphQL implementation",
+				"Sending virtual gifts"
+			],
+			tags: [
+				"JavaScript",
+				"TypesScript",
+				"React-Native",
+				"Redux",
+				"Saga",
+				"Firebase",
+				"Jenkins",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "RCSL",
+			duration: "March 2018 - December 2018",
+			subtitle: "Mobile Development Intern",
+			details: [
+				"Worked as a react native developer to support tech team.",
+				"Developed a customer Mobile Application from scratch for social media management.",
+			],
+			tags: ["JavaScript", "TypesScript", "React-Native"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Information Technology and Engineering",
+			duration: "June 2016 - March 2020",
+			subtitle: "Gandhinagar Institute Of Technology, Gandhinagar",
+			details: [
+				"Active Competitive Programmer with CodeChef Rating 1841*.",
+				"Received 500+ stars and 300+ forks on GitHub projects.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System",
+				"Computer Networks",
+				"Compiler Designing",
+				"Cloud Computing",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Class 11-12th in Science and Mathematics",
+			duration: "June 2014 - March 2016",
+			subtitle: "Board of Secondary Education, Gujarat",
+			details: [
+				"Qualified JEE Main.",
+				"Secured 96.5 percentile in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/dhruv161",
+				},
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/10738208/dhruv-pandya",
+				}
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://in.linkedin.com/in/dhruv-pandya-545a0b144",
+				},
+				{
+					text: "Instagram",
+					link: "https://www.instagram.com/reactnativemobile/",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Dhruv Pandya.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
